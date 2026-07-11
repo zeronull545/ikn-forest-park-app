c1168ee0bc89c293729958d6bf6b7088f25f97af
zeronull545 <35220186+zeronull545@users.noreply.github.com>
2026-07-11 08:43:24 +0700

    feat: add app/Models/Like.php

---
+++ app/Models/Like.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
+
+    /**
+     * Get the post that is liked.
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    /**
+     * Get the user who liked the post.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
