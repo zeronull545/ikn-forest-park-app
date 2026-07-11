89c403fcffc068a77ad68d273beee3733df1d516
zeronull545 <35220186+zeronull545@users.noreply.github.com>
2026-07-11 08:43:23 +0700

    feat: add app/Models/Comment.php

---
+++ app/Models/Comment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content',
+    ];
+
+    /**
+     * Get the post this comment is associated with.
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    /**
+     * Get the user who made the comment.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
